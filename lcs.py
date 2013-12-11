1e7302c172f2c7b7b72f3cdac62425dc4ac14937
Kavin Yao <kavinyao@gmail.com>
2013-12-10 16:23:08 -0800

    add longest common subsequence code

---
+++ lcs.py
@@ -0,0 +1,31 @@
+# Code from: http://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Longest_common_subsequence#Python
+
+def longest_common_subsequence(X, Y):
+    m = len(X)
+    n = len(Y)
+    C = LCS(X, Y)
+    return backTrack(C, X, Y, m, n)
+
+def LCS(X, Y):
+    m = len(X)
+    n = len(Y)
+    # An (m+1) times (n+1) matrix
+    C = [[0] * (n+1) for i in range(m+1)]
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            if X[i-1] == Y[j-1]:
+                C[i][j] = C[i-1][j-1] + 1
+            else:
+                C[i][j] = max(C[i][j-1], C[i-1][j])
+    return C
+
+def backTrack(C, X, Y, i, j):
+    if i == 0 or j == 0:
+        return ""
+    elif X[i-1] == Y[j-1]:
+        return backTrack(C, X, Y, i-1, j-1) + X[i-1]
+    else:
+        if C[i][j-1] > C[i-1][j]:
+            return backTrack(C, X, Y, i, j-1)
+        else:
+            return backTrack(C, X, Y, i-1, j)
